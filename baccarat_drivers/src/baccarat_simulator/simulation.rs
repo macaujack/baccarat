d0ad351b620b7aab27acd483f46d6e51ed49a6ee
Jack <seigino.mikata@outlook.com>
2023-07-06 13:22:48 +0800

    Aggregate log data and print together
    Now log is printed only when gambler places bets.

--- baccarat_drivers/src/baccarat_simulator/simulation.rs
+++ baccarat_drivers/src/baccarat_simulator/simulation.rs
@@ -2,14 +2,14 @@ use baccarat::calculation::Solution;
 use baccarat::card::{Card, Shoe};
 use baccarat::game::{DealerProvider, GamblerProvider, Game, Hand, HandsBet, RoundResult};
 use baccarat::Rule;
-use baccarat_drivers_lib::ConfigBaccaratSimulator;
+use baccarat_drivers_lib::{ConfigBaccaratSimulator, MoneyStat};
 use std::cell::RefCell;
 use std::collections::HashMap;
 
 pub fn start_simulation(rule: &Rule, config: &ConfigBaccaratSimulator) {
-    let print_firsts = RefCell::new(false);
-    let dealer = SimulatorDealer::new(rule, &print_firsts);
-    let gambler = SimulatorGambler::new(rule, config, &print_firsts);
+    let firsts = RefCell::new(None);
+    let dealer = SimulatorDealer::new(rule, &firsts);
+    let gambler = SimulatorGambler::new(rule, config, &firsts);
     let mut game = Game::new(rule, dealer, gambler);
     game.start_game_loop();
 }
@@ -18,32 +18,79 @@ pub fn start_simulation(rule: &Rule, config: &ConfigBaccaratSimulator) {
 struct SimulatorGambler<'a> {
     _rule: &'a Rule,
     config: &'a ConfigBaccaratSimulator,
-    print_firsts: &'a RefCell<bool>,
+    firsts: &'a RefCell<Option<Vec<Card>>>,
 
     bets: HashMap<HandsBet, i64>,
 
     rounds: u32,
-    money: i64,
-    min_money: i64,
+
+    // Records for log
+    max_bets: ((HandsBet, f64), (HandsBet, f64)),
+    money_stat: MoneyStat,
 }
 
 impl<'a> SimulatorGambler<'a> {
     fn new(
         _rule: &'a Rule,
         config: &'a ConfigBaccaratSimulator,
-        print_firsts: &'a RefCell<bool>,
+        firsts: &'a RefCell<Option<Vec<Card>>>,
     ) -> Self {
         Self {
             _rule,
             config,
-            print_firsts,
+            firsts,
 
             bets: HashMap::new(),
 
             rounds: 0,
-            money: 0,
-            min_money: 0,
+
+            max_bets: (
+                (HandsBet::BankerWin, -f64::INFINITY),
+                (HandsBet::PlayerBonus, -f64::INFINITY),
+            ),
+            money_stat: Default::default(),
         }
+    }
+
+    fn print_log(&self, delta_money: i64, final_player: &Hand, final_banker: &Hand) {
+        println!(
+            "Day #{}, Round #{}",
+            self.rounds / self.config.rounds_per_day,
+            self.rounds % self.config.rounds_per_day
+        );
+
+        println!(
+            "Max main bet: {:#?}({:.5}). Max side bet: {:#?}({:.5})",
+            self.max_bets.0 .0, self.max_bets.0 .1, self.max_bets.1 .0, self.max_bets.1 .1
+        );
+
+        print!("Bet:");
+        for (bet, money) in &self.bets {
+            print!(" ({:#?}, {})", bet, money);
+        }
+        println!();
+
+        let mut firsts = self.firsts.borrow_mut();
+        let mut tem = None;
+        std::mem::swap(&mut *firsts, &mut tem);
+        let firsts = tem.unwrap();
+        print!("First {} cards in shoe:", firsts.len());
+        for card in firsts {
+            print!(" {:#?}", card);
+        }
+        println!();
+
+        println!("Final hands: {:#?} {:#?}", final_player, final_banker);
+
+        println!(
+            "Money: {}({}). Min: {}. Max: {}.",
+            self.money_stat.cur_money(),
+            delta_money,
+            self.money_stat.min_money(),
+            self.money_stat.max_money()
+        );
+
+        println!("----------------------------------");
     }
 }
 
@@ -51,8 +98,8 @@ impl<'a> GamblerProvider for SimulatorGambler<'a> {
     fn on_new_shoe(&mut self) {
         println!("NEW SHOE!!!!!!!");
         println!("++++++++++++++++++++++++++++++++++");
-        let mut print_firsts = self.print_firsts.borrow_mut();
-        *print_firsts = true;
+        let mut firsts = self.firsts.borrow_mut();
+        *firsts = None;
     }
     fn on_cut_card_reached(&mut self, _cards_before_cut: u32) {}
     fn on_discard(&mut self, card: Card) {
@@ -60,22 +107,14 @@ impl<'a> GamblerProvider for SimulatorGambler<'a> {
         println!("++++++++++++++++++++++++++++++++++");
     }
     fn on_round_start(&mut self) {
-        println!(
-            "Day #{}, Round #{}",
-            self.rounds / self.config.rounds_per_day,
-            self.rounds % self.config.rounds_per_day
-        );
-        let mut print_firsts = self.print_firsts.borrow_mut();
-        *print_firsts = true;
+        let mut firsts = self.firsts.borrow_mut();
+        *firsts = None;
     }
     fn place_bet(&mut self, solution: &Solution) -> &HashMap<HandsBet, i64> {
         self.bets.clear();
 
         let max_bets = solution.get_max_main_side_bets();
-        println!(
-            "Max main bet: {:#?}({:.5}). Max side bet: {:#?}({:.5})",
-            max_bets.0 .0, max_bets.0 .1, max_bets.1 .0, max_bets.1 .1
-        );
+        self.max_bets = max_bets;
 
         // If side bet's ex < main bet's ex, we don't consider side bets.
         if max_bets.1 .1 < max_bets.0 .1 {
@@ -91,27 +130,13 @@ impl<'a> GamblerProvider for SimulatorGambler<'a> {
             }
         }
 
-        print!("Bet:");
-        for (bet, money) in &self.bets {
-            print!(" ({:#?}, {})", bet, money);
-        }
-        println!();
-
         &self.bets
     }
     fn on_round_end(&mut self, player: &Hand, banker: &Hand, round_result: &RoundResult) {
-        println!("Final hands: {:#?} {:#?}", player, banker);
-        self.money += round_result.total_profit;
-        let mut delta_min_money = 0;
-        if self.money < self.min_money {
-            delta_min_money = self.money - self.min_money;
-            self.min_money = self.money;
+        self.money_stat.add(round_result.total_profit);
+        if self.bets.len() != 0 {
+            self.print_log(round_result.total_profit, player, banker);
         }
-        println!(
-            "Money: {}({}). Min: {}({})",
-            self.money, round_result.total_profit, self.min_money, delta_min_money
-        );
-        println!("----------------------------------");
         self.rounds += 1;
     }
 }
@@ -120,34 +145,29 @@ impl<'a> GamblerProvider for SimulatorGambler<'a> {
 struct SimulatorDealer<'a> {
     _rule: &'a Rule,
     shoe: Shoe,
-    print_firsts: &'a RefCell<bool>,
+    firsts: &'a RefCell<Option<Vec<Card>>>,
 }
 
 impl<'a> SimulatorDealer<'a> {
-    fn new(rule: &'a Rule, print_firsts: &'a RefCell<bool>) -> Self {
+    fn new(rule: &'a Rule, firsts: &'a RefCell<Option<Vec<Card>>>) -> Self {
         Self {
             _rule: rule,
             shoe: Shoe::new(rule.number_of_decks, rule.cut_card_proportion),
-            print_firsts,
+            firsts,
         }
     }
 }
 
 impl<'a> DealerProvider for SimulatorDealer<'a> {
     fn deal_card(&mut self) -> Card {
-        let mut print_firsts = self.print_firsts.borrow_mut();
-        if *print_firsts {
-            *print_firsts = false;
+        let mut firsts = self.firsts.borrow_mut();
+        if firsts.is_none() {
             let nexts = self.shoe.get_next_cards();
             const NUMBER: usize = 10;
-            print!("First {} cards in shoe:", NUMBER);
-            for (i, card) in nexts.iter().enumerate() {
-                if i == NUMBER {
-                    break;
-                }
-                print!(" {:#?}", card);
-            }
-            println!();
+            let len = std::cmp::min(nexts.len(), NUMBER);
+            let mut v = vec![Default::default(); len];
+            v.clone_from_slice(&nexts[..len]);
+            *firsts = Some(v);
         }
 
         self.shoe.deal_card()
--- baccarat_drivers/src/lib.rs
+++ baccarat_drivers/src/lib.rs
@@ -46,3 +46,44 @@ pub fn parse_config_from_file(filename: &str) -> Config {
     let file_content = std::fs::read_to_string(filename).unwrap();
     serde_yaml::from_str(&file_content).unwrap()
 }
+
+#[derive(Debug, Clone)]
+pub struct MoneyStat {
+    cur: i64,
+    min: i64,
+    max: i64,
+}
+
+impl Default for MoneyStat {
+    fn default() -> Self {
+        Self {
+            cur: 0,
+            min: i64::MAX,
+            max: i64::MIN,
+        }
+    }
+}
+
+impl MoneyStat {
+    pub fn add(&mut self, delta: i64) {
+        self.cur += delta;
+        if self.min > self.cur {
+            self.min = self.cur;
+        }
+        if self.max < self.cur {
+            self.max = self.cur;
+        }
+    }
+
+    pub fn cur_money(&self) -> i64 {
+        self.cur
+    }
+
+    pub fn min_money(&self) -> i64 {
+        self.min
+    }
+
+    pub fn max_money(&self) -> i64 {
+        self.max
+    }
+}
